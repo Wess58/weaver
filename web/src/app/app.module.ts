6e85b53988f7c7ed1cf2b0fd8ea16c4ea2e3edda
wess58 <wesleymbate@gmail.com>
2025-06-08 17:59:55 +0300

    generated intial components added libraries : material19 , bootstrap5 and fontawesome6.7

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { PropertyListComponent } from './components/property/property-list/property-list.component';
+import { PropertyDetailComponent } from './components/property/property-detail/property-detail.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyListComponent,
+    PropertyDetailComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ web/src/app/components/property/property-detail/property-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-detail',
+  standalone: false,
+  templateUrl: './property-detail.component.html',
+  styleUrl: './property-detail.component.scss'
+})
+export class PropertyDetailComponent {
+
+}
