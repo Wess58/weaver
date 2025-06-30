d742c130422f301301d694006566f7a761052a93
kathukyabrian <briankathukya2000@gmail.com>
2025-06-30 21:32:42 +0300

    added basic CRUD operations for property

--- api/src/main/java/tech/weki/weaver/api/web/rest/PropertyResource.java
+++ api/src/main/java/tech/weki/weaver/api/web/rest/PropertyResource.java
@@ -6,19 +6,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.weki.weaver.api.domain.Property;
 import tech.weki.weaver.api.service.PropertyService;
 import tech.weki.weaver.api.web.util.PaginationUtil;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 public class PropertyResource {
+    private final String BASE_URL = "/api/properties";
     private final PropertyService propertyService;
 
     public PropertyResource(PropertyService propertyService) {
@@ -31,5 +31,12 @@ public class PropertyResource {
         Page<Property> page = propertyService.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/properties");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+
+    @PostMapping("/properties")
+    public ResponseEntity<Property> save(@RequestBody Property property) {
+        log.info("REST request to save property: {}", property);
+        property = propertyService.save(property);
+        return ResponseEntity.created(URI.create(BASE_URL + "/" + property.getId())).body(property);
     }
 }
